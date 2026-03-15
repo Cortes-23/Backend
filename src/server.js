22add2866a5781ce87617e2a5e1c1dfa552689f8
David Cortes <steveen.cortess@gmail.com>
2026-03-15 18:28:44 -0500

    Subiendo backend

---
+++ src/server.js
@@ -0,0 +1,43 @@
+
+import express from "express"
+import mongoose from "mongoose"
+import cors from "cors"
+import dotenv from "dotenv"
+
+import authRoutes from "./routes/auth.routes.js"
+import userRoutes from "./routes/userRoutes.js"
+import clienteRoutes from "./routes/cliente.routes.js"
+import creditoRoutes from "./routes/credito.routes.js"
+import officeRoutes from "./routes/oficina.routes.js"
+import superadminRoutes from "./routes/Superadmin.routes.js"
+
+dotenv.config()
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+/* RUTAS */
+app.use("/api/auth", authRoutes)
+app.use("/api/users", userRoutes)
+app.use("/api/clientes", clienteRoutes)
+app.use("/api/creditos", creditoRoutes)
+app.use("/api/oficinas", officeRoutes)
+app.use("/api/superadmin", superadminRoutes)
+
+/* CONEXION MONGODB */
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+  console.log("✅ Conectado a MongoDB")
+})
+.catch((error) => {
+  console.log("❌ Error conectando MongoDB")
+  console.log(error)
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+  console.log(`🚀 Servidor corriendo en puerto ${PORT}`)
+})
